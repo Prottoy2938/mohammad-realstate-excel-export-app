90e8fd364522ffd1d665123dad8143f3677ce393
Alred <prottoyrudra7777@gmail.com>
2024-02-16 20:28:37 +0000

    did some stuf

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 
-import { Background } from '../background/Background';
 import { Button } from '../button/Button';
 import { HeroOneButton } from '../hero/HeroOneButton';
 import { Section } from '../layout/Section';
@@ -8,7 +7,7 @@ import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 import { Logo } from './Logo';
 
 const Hero = () => (
-  <Background color="bg-gray-100">
+  <div color="bg-gray-100">
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />}>
         <li>Home</li>
@@ -18,20 +17,28 @@ const Hero = () => (
         </li>
       </NavbarTwoColumns>
     </Section>
-
-    <Section yPadding="pt-20 pb-32">
-      <HeroOneButton
-        title={
-          <>
-            {'Streamline Real estate\n'}
-            <span className="text-primary-500">Valuation</span>
-          </>
-        }
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-        button={<Button xl>Get Started</Button>}
-      />
-    </Section>
-  </Background>
+    <div
+      style={{
+        backgroundImage:
+          'url(https://images.pexels.com/photos/460695/pexels-photo-460695.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <Section yPadding="pt-20 pb-32">
+        <HeroOneButton
+          title={
+            <>
+              <span className="text-white">{'Streamline Real estate\n'}</span>
+              <span className="text-primary-500">Valuation</span>
+            </>
+          }
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          button={<Button xl>Get Started</Button>}
+        />
+      </Section>
+    </div>
+  </div>
 );
 
 export { Hero };
