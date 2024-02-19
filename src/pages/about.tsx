9cc14d71594f7807dca88ac48645718c2d46a2e5
Alred <prottoyrudra7777@gmail.com>
2024-02-19 10:22:29 +0000

    created bunch of pages

---
+++ src/pages/about.tsx
@@ -0,0 +1,16 @@
+import { Banner } from '@/templates/Banner';
+import { VerticalFeatures } from '@/templates/VerticalFeatures';
+
+import { Footer } from '../templates/Footer';
+import { Hero } from '../templates/Hero';
+
+const Index = () => (
+  <>
+    <Hero />
+    <VerticalFeatures />
+    <Banner />
+    <Footer />
+  </>
+);
+
+export default Index;
--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -1,7 +1,5 @@
 import Link from 'next/link';
 
-import { Button } from '../button/Button';
-import { HeroOneButton } from '../hero/HeroOneButton';
 import { Section } from '../layout/Section';
 import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 import { Logo } from './Logo';
@@ -10,14 +8,23 @@ const Hero = () => (
   <div color="bg-gray-100">
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />}>
-        <li>Home</li>
-
+        <li>
+          <Link href="/">Home</Link>
+        </li>       
+        <li>
+          <Link href="/contact">Contact Us</Link>
+        </li>
+        <li>
+          <Link href="/blogs">Blogs</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
         <li>
           <Link href="/signin">Sign in</Link>
         </li>
       </NavbarTwoColumns>
     </Section>
-   
   </div>
 );
 
