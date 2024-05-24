3a056f8ebb6234927b6e4ae97e0c49af4962984b
richardgreenss <166267537+richardgreenss@users.noreply.github.com>
2024-05-24 23:00:31 +0000

    fixed some config file

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,12 +5,13 @@ import { initializeApp, getApps } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyANdAgxXwRsE1H7xD-QTiYJWiWFsmUndyk",
+  authDomain: "realestate-project-d2fb8.firebaseapp.com",
+  projectId: "realestate-project-d2fb8",
+  storageBucket: "realestate-project-d2fb8.appspot.com",
+  messagingSenderId: "1088768967114",
+  appId: "1:1088768967114:web:fa1ae07d3645bf28764f33",
+  measurementId: "G-BN7WQB8ENQ"
 };
 
 // Initialize Firebase
