a143236356df988900d7e2f697693cbc7d457f95
richardgreenss <166267537+richardgreenss@users.noreply.github.com>
2024-06-12 10:05:53 +0000

    updated algolia indecides

--- src/pages/my-clients.tsx
+++ src/pages/my-clients.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   Button,
   Heading,
+  Spinner,
   Table,
   Tbody,
   Td,
@@ -86,7 +87,20 @@ const Clients = () => {
   };
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <Box>
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="blue.500"
+          size="xl"
+          pos="fixed"
+          top={'45vh'}
+          left={'48vw'}
+        />
+      </Box>
+    );
   }
 
   return (
