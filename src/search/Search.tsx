0204af7096755af9feb469ca7c39846a3c363887
richardgreenss <166267537+richardgreenss@users.noreply.github.com>
2024-06-12 10:04:31 +0000

    updated algolia indecides

--- src/search/Search.tsx
+++ src/search/Search.tsx
@@ -1,16 +1,20 @@
-import algoliasearch from "algoliasearch/lite";
-import "instantsearch.css/themes/satellite.css";
-import { Hits, InstantSearch, SearchBox, Configure } from "react-instantsearch";
+import 'instantsearch.css/themes/satellite.css';
 
-import { Hit } from "./Hit";
+import algoliasearch from 'algoliasearch/lite';
+import { Configure, Hits, InstantSearch, SearchBox } from 'react-instantsearch';
 
-const searchClient = algoliasearch("VN9PEMX9WY", "b73be6df973815e774a0976280c8c80f");
+import { Hit } from './Hit';
+
+const searchClient = algoliasearch(
+  'VN9PEMX9WY',
+  'b73be6df973815e774a0976280c8c80f',
+);
 
 export const Search = () => {
   return (
     <InstantSearch
       searchClient={searchClient}
-      indexName="firebase-data"
+      indexName="FIREBASE_ALGOLIA-SEARCH"
     >
       <Configure hitsPerPage={5} />
       <div className="ais-InstantSearch">
